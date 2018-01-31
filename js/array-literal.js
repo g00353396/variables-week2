83d3c6274772db5d6453cd4f52f647c5a1e11b5b
g00353396 <36000701+g00353396@users.noreply.github.com>
2018-01-31 14:33:04 +0000

    Version2

--- js/array-literal.js
+++ js/array-literal.js
@@ -1,8 +1,12 @@
 // Create an array and assign it values
 var colors = new Array('white','black','red');
 colors.push("purple");
+var days = new Array('Monday',"Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday");
+
 
 
 // Show the first item from the array
 var el = document.getElementById('colors');
 el.textContent = colors[3];
+var d = document.getElementById('days');
+d.textContent = days[2];
